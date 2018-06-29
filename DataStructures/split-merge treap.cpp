484bfff24df32aba34c608d0316bd2900e098703
Abedalmjeed Abutaleb <icarbon95@outlook.com>
2018-06-29 15:24:34 +0300

    fix issue with lazy, and got AC using it in Uva 11922

--- DataStructures/split-merge treap.cpp
+++ DataStructures/split-merge treap.cpp
@@ -32,72 +32,72 @@ const ll MOD = 1e9 + 7;
 const double eps = 1e-9;
 
 struct node {
-	int v,p,sz;
+	int v, p, sz;
 	bool lazy;
-	node * L, * R;
-	
+	node * L, *R;
+
 	node() {
 		p = rand() % int(1e9);
 		L = NULL;
 		R = NULL;
 		sz = 1;
 	}
-	
-	node(int v): node() {
+
+	node(int v) : node() {
 		this->v = v;
 	}
 };
 
 struct treap {
 	node * root;
-	
+
 	treap() {
 		root = NULL;
 		srand(time(NULL));
 	}
-	
+
 	treap(node * root) {
 		this->root = root;
 		srand(time(NULL));
 	}
-	
+
 	node * rotate_left(node * R) {
-		node * y = R -> R;
-		node * T2 = y -> L;
-		R -> R = T2;
-		y -> L = R;
+		node * y = R->R;
+		node * T2 = y->L;
+		R->R = T2;
+		y->L = R;
 		fix(R), fix(y);
 		return y;
 	}
-	
+
 	node * rotate_right(node * R) {
-		node * x = R -> L;
-		node * T2 = x -> R;
-		R -> L = T2;
-		x -> R = R;
+		node * x = R->L;
+		node * T2 = x->R;
+		R->L = T2;
+		x->R = R;
 		fix(R), fix(x);
 		return x;
 	}
 
 	inline int get_size(node * R) {
-		return R == NULL ? 0 : R -> sz;
+		return R == NULL ? 0 : R->sz;
 	}
 
 	inline void fix(node * R) {
-		if(R == NULL) return;
-		R -> sz = get_size(R -> L) + get_size(R -> R) + 1;
+		if (R == NULL) return;
+		R->sz = get_size(R->L) + get_size(R->R) + 1;
 	}
 
 	void push(node * R) {
-		if(R == NULL)
+		if (R == NULL)
 			return;
-		if(R -> lazy) {
-			swap(R -> L, R -> R);
-			if(R -> L != NULL)
-				R -> L -> lazy ^= 1;
-			if(R -> R != NULL)
-				R -> R -> lazy ^= 1;
-			R -> lazy = 0;
+		if (R->lazy) {
+			swap(R->L, R->R);
+			if (R->L != NULL)
+				R->L->lazy ^= 1;
+			if (R->R != NULL)
+				R->R->lazy ^= 1;
+			R->lazy = 0;
 		}
 	}
 
@@ -106,19 +106,20 @@ struct treap {
 	}
 
 	node * insert(node * cur, int k, node * newNode) {
-		if(cur == NULL)
+		if (cur == NULL)
 			return newNode;
-		
+
 		push(cur);
-		
-		int tmp = get_size(cur -> L) + 1;
-		if(tmp >= k) {
-			cur -> L = insert(cur -> L, k, newNode);
-			if((cur -> L == NULL ? 0 : cur -> L -> p) > cur -> p)
+
+		int tmp = get_size(cur->L) + 1;
+		if (tmp >= k) {
+			cur->L = insert(cur->L, k, newNode);
+			if ((cur->L == NULL ? 0 : cur->L->p) > cur->p)
 				cur = rotate_right(cur);
-		} else {
-			cur -> R = insert(cur -> R, k - tmp, newNode);
-			if((cur -> R == NULL ? 0 : cur -> R -> p) > cur -> p)
+		}
+		else {
+			cur->R = insert(cur->R, k - tmp, newNode);
+			if ((cur->R == NULL ? 0 : cur->R->p) > cur->p)
 				cur = rotate_left(cur);
 		}
 
@@ -127,79 +128,82 @@ struct treap {
 	}
 
 	void insert(int v, int k) { // insert value v at index k (1-based indexed) 
-		root = insert(root,k,new node(v));
-	} 
+		root = insert(root, k, new node(v));
+	}
 
 	void erase(int k) {
 		push(root);
-		pair<treap,treap> u1 = split(k);
-		pair<treap,treap> u2 = u1.second.split(2);
+		pair<treap, treap> u1 = split(k);
+		pair<treap, treap> u2 = u1.second.split(2);
 		root = u1.first.merge(u2.second).root;
-		fix(root); 
+		fix(root);
 	}
 
-	pair<treap,treap> split(node * cur, int k) {
+	pair<treap, treap> split(node * cur, int k) {
 		node * newNode = new node(-1);
-		newNode -> p = 2e9;
+		newNode->p = 2e9;
 		node * temp = insert(root, k, newNode);
-		node * a = temp -> L, * b = temp -> R;
+		node * a = temp->L, *b = temp->R;
+		push(a), push(b);
 		fix(a), fix(b);
 		delete temp;
-		return make_pair(treap(a),treap(b));
+		return make_pair(treap(a), treap(b));
 	}
 
 	//split treap [1...N] => [1...k-1], [k...N] 
-	pair<treap,treap> split(int k) { 
-		if(k < 1) return make_pair(treap(),*this);
-		else if(k > get_size(root)) return make_pair(*this,treap());
+	pair<treap, treap> split(int k) {
+		if (k < 1) return make_pair(treap(), *this);
+		else if (k > get_size(root)) return make_pair(*this, treap());
 		return split(root, k);
 	}
 
-	node * merge(node * a,node * b) {
-		if(a == NULL) return b;
-		if(b == NULL) return a;
-		if(a -> v > b -> v) {
-			a -> R = merge(a -> R, b);
+	node * merge(node * a, node * b) {
+		if (a == NULL) return b;
+		if (b == NULL) return a;
+		push(a), push(b);
+		if (a->v > b->v) {
+			a->R = merge(a->R, b);
 			fix(a);
 			return a;
-		} else{
-			b -> L = merge(a, b -> L);
+		}
+		else {
+			b->L = merge(a, b->L);
 			fix(b);
 			return b;
 		}
 	}
 
 	treap merge(treap o) {
-		return treap(merge(root,o.root));
+		return treap(merge(root, o.root));
 	}
 
 	void reverse() {
-		if(root == NULL)
+		if (root == NULL)
 			return;
-		root -> lazy ^= 1;
+		root->lazy ^= 1;
 		push(root);
 	}
 
 	int get_kth(node * cur, int k) {
-		if(cur == NULL)
+		if (cur == NULL)
 			return -1;
 
 		push(cur);
 
-		int tmp = get_size(cur -> L) + 1;
+		int tmp = get_size(cur->L) + 1;
 
-		if(k == tmp)
-			return cur -> v;
+		if (k == tmp)
+			return cur->v;
 
-		if(k < tmp)
-			return get_kth(cur -> L, k);
-		return get_kth(cur -> R, k - tmp);
+		if (k < tmp)
+			return get_kth(cur->L, k);
+		return get_kth(cur->R, k - tmp);
 	}
 
 	int get_kth(int k) {
 		return get_kth(root, k);
 	}
-
+	
 	void print() {
 		int n = get_size(root);
 		for(int i=1; i<=n; i++)
